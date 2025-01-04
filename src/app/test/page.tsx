0fb89df15648ab1a202a1cd7b120b956ba34723d
Korrectional <156260369+korrectional@users.noreply.github.com>
2025-01-04 18:11:02 -0500

    Setup 1

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-//import Message from './Message'
-import Message2 from './Message2'
-function App(){
-  // JSX: Javascript XML
-  return <div><Message2/></div>;
-}
-
-export default App
--- src/lib/auth-client.ts
+++
@@ -1,9 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-import { inferAdditionalFields } from "better-auth/client/plugins";
-import { clientEnvs } from "@/env/client";
-import type { auth } from "./auth";
-
-export const authClient = createAuthClient({
-  plugins: [inferAdditionalFields<typeof auth>()],
-  baseURL: clientEnvs.NEXT_PUBLIC_DOMAIN,
-});
