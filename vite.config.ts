68e0f14be7dab497211156db445c4cbfb5698f5a
Korrectional <156260369+korrectional@users.noreply.github.com>
2025-01-14 22:38:59 -0500

    Sign up - Sign in

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+<<<<<<< Updated upstream
     port: 5173,
+=======
     port: 3000, 
+>>>>>>> Stashed changes
   }
 })
